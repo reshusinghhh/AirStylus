10659df2f76914803734322158508265c533620d
Guneet Kaur <shefalikharbanda@gmail.com>
2018-10-28 15:34:56 +0530

    Window Created

---
+++ AirStylusWindow/main.py
@@ -0,0 +1,31 @@
+from tkinter import *
+
+
+root = Tk(className="Air Stylus")
+
+
+root.configure(background='white', cursor="arrow")
+root.geometry("1280x720")
+root.resizable(width=True, height=True)
+
+h = StringVar()
+heading = Label(root, textvariable=h, fg='red')
+
+
+
+h.set('AIR STYLUS')
+heading.configure(font=('Ethocentric', 20))
+heading.pack()
+
+
+
+pre = StringVar()
+present = Label(root, textvariable=pre)
+pre.set('Presented to you by:-\nGuneet Kaur\nReshu Singh\nAmisha Rawat\nCharanjeet Kaur')
+present.configure(font=('Helvetica', 15))
+present.pack(side=BOTTOM)
+
+textBox = Text(root, height=1100, width=600)
+textBox.pack()
+
+root.mainloop()
